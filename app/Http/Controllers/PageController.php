adf9a5b52f11728cc78412ca6ea18a10ecfbc36f
AndreaParrini <andrea.parrini26@gmail.com>
2024-04-26 13:23:19 +0200

    modify route and controller add function index in controller and use it in route home use Model Train to recover by db all train of the current day

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        $trainsOfTheDay = Train::whereDate('departure_time', date('Y-m-d'))->get();
+        //dd($trainsOfTheDay);
+        return view('welcome', compact('trainsOfTheDay'))->name('home');
+    }
 }
