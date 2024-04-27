8574b24dc2fd94948766ab7cabab06483c605837
AndreaParrini <andrea.parrini26@gmail.com>
2024-04-27 14:09:14 +0200

    add card in welcome page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
 {
     public function index()
     {
-        $trainsOfTheDay = Train::whereDate('departure_time', date('Y-m-d') /* '2024-04-26' */)->get();
+        $trainsOfTheDay = Train::whereDate('departure_time', /* date('Y-m-d')  */ '2024-04-26')->get();
         //dd($trainsOfTheDay);
         return view('welcome', compact('trainsOfTheDay'));
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,18 +3,62 @@
 @section('PageTitle', 'Home Page')
 
 @section('content')
-    <div class="container text-light">
-        <div class="row">
-            @forelse ($trainsOfTheDay as $train)
-                <div class="col-4">
-                    {{ $train->company }}
-                </div>
-            @empty
-                <div class="col">
-                    Non ci sono Treni in partezza per la data odierna
-                </div>
-            @endforelse
+    <section id="section_main" class="p-5">
+        <div class="container text-light">
+            <div class="row gap-4">
+                @forelse ($trainsOfTheDay as $train)
+                    <div class="col-12 ">
+                        <div class="card">
+                            <div class="card-body {{ $train->isCancelled ? 'text-decoration-line-through' : '' }}">
+                                <h5 class="card-title text-center">{{ $train->company }} {{ $train->train_code }}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted text-center">{{ $train->type_of_trains }}</h6>
+                                @if ($train->isCancelled)
+                                    <div class="text-center">
+                                        <span class="badge bg-danger text-center">Cancellato</span>
+                                    </div>
+                                @endif
+                                <div class="d-flex justify-content-between align-items-center m-2">
+                                    <div class="w-25">
+                                        <h6 class="fw-bold">Stazione di Partenza: {{ $train->departure_station }}</h6>
+                                        <span>{{ date('d-m-Y', strtotime($train->departure_time)) }} alle
+                                            <span
+                                                class="fw-bold fs-5">{{ date('H:m', strtotime($train->departure_time)) }}</span>
+                                        </span>
+                                        <div>
+                                            <span class="badge bg-info">Binario: {{ $train->strating_platform }}</span>
+                                        </div>
 
+
+                                    </div>
+                                    <div class="d-flex flex-column gap-4 align-items-center">
+                                        <i class="fa-solid fa-arrow-right fa-2xl" style="color: #000000;"></i>
+                                        <div class="badge bg-primary">{{ $train->price }} €</div>
+                                    </div>
+                                    <div class="w-25">
+                                        <h6 class="fw-bold">Stazione di Arrivo: {{ $train->arrival_station }}</h6>
+                                        <span>{{ date('d-m-Y', strtotime($train->arrival_time)) }} alle
+                                            <span
+                                                class="fw-bold fs-5">{{ date('H:m', strtotime($train->arrival_time)) }}</span>
+                                        </span>
+                                        <div>
+                                            <span class="badge bg-info">Binario: {{ $train->arrival_platform }}</span>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                @empty
+                    <div class="col">
+                        Non ci sono Treni in partezza per la data odierna
+                    </div>
+                @endforelse
+
+            </div>
         </div>
-    </div>
+    </section>
+
 @endsection
